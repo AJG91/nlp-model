ad5e1932c9ad2e40da4dc453f96344c9b94a0d90
Alberto J Garcia <albertojg1991@gmail.com>
2025-11-01 13:03:31 -0700

    Added docstring and type hinting to functions.

--- src/nlp_model/metrics.py
+++ src/nlp_model/metrics.py
@@ -1,8 +1,22 @@
+import torch as tc
 from sklearn.metrics import accuracy_score, f1_score
 
 def compute_metrics(
-    eval_pred
-):
+    eval_pred: tuple[tc.tensor, tc.tensor]
+) -> dict[str, float]:
+    """
+    Compute evaluation metrics (accuracy and weighted F1 score) for model predictions.
+
+    Parameters
+    ----------
+    eval_pred : tuple[tc.tensor, tc.tensor]
+        A tuple containing predictions (output logits or probabilities) and labels (ground-truth) of the evaluation phase.
+
+    Returns
+    -------
+    metrics : dict[str, float]
+        A dictionary containing accuracy and F1 score.
+    """
     predictions, labels = eval_pred
     preds = predictions.argmax(axis=-1)
     return {
--- src/nlp_model/plots.py
+++ src/nlp_model/plots.py
@@ -1,12 +1,28 @@
+import numpy as np
 from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 def compute_confusion_matrix(
-    y_true, 
-    y_pred,
-    path,
-    dpi
+    y_true: np.ndarray, 
+    y_pred: np.ndarray,
+    path: str,
+    dpi: int
 ) -> None:
+    """
+    Compute and save a confusion matrix plot for binary classification results.
+
+    Parameters
+    ----------
+    y_true : np.ndarray
+        True class labels.
+    y_pred : np.ndarray
+        Predicted class labels. Must have the same shape as `y_true`.
+    path : str
+        String to the directory where the plot is saved.
+    dpi : int
+        Dots per inch.
+        A higher dpi results in a sharper image.
+    """
     cm = confusion_matrix(y_true, y_pred)
     disp = ConfusionMatrixDisplay(cm, display_labels=["Negative", "Positive"])
     disp.plot()
